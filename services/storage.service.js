f071e0a9cb0de0f05fa48cf59ec8b54029e895f8
Евений Зайцев <evenijzajcev@192.168.0.12>
2023-04-22 15:47:34 +0700

    Добавлен storage service

---
+++ services/storage.service.js
@@ -0,0 +1,8 @@
+import { homedir } from "os";
+import { join, basename } from "path";
+
+const filePath = join(homedir(), "./weather_data.json");
+
+export function saveKeyValue(key, value) {
+  console.log(basename(filePath));
+}
