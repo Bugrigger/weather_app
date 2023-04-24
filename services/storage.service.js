a234dca714c91d0633447dbd3b7e2b353e173fde
evgeny_zaytsev <wispi@list.ru>
2023-04-24 21:06:58 +0700

    Добавлен сервис обрещения к апи

--- services/storage.service.js
+++ services/storage.service.js
@@ -1,8 +1,38 @@
 import { homedir } from "os";
 import { join, basename } from "path";
+import { promises } from "fs";
 
 const filePath = join(homedir(), "./weather_data.json");
+export const TOKEN_DICTIONARY = {
+  token: "token",
+  city: "city",
+};
 
-export function saveKeyValue(key, value) {
-  console.log(basename(filePath));
+export async function saveKeyValue(key, value) {
+  let data = {};
+
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    data = JSON.parse(file);
+  }
+  data[key] = value;
+  await promises.writeFile(filePath, JSON.stringify(data));
+}
+
+export async function getKeyValue(key) {
+  if (await isExist(filePath)) {
+    const file = await promises.readFile(filePath);
+    const data = JSON.parse(file);
+    return data[key];
+  }
+  return undefined;
+}
+
+async function isExist(path) {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch {
+    return false;
+  }
 }
--- weather.js
+++ weather.js
@@ -1,8 +1,22 @@
 // #!/usr/bin/env node
 
 import { getArgs } from "./helpers/args.js";
-import { pringHelp } from "./services/log.service.js";
-import { saveKeyValue } from "./services/storage.service.js";
+import { getWeather } from "./services/api.service.js";
+import { pringHelp, printError, printSuccess } from "./services/log.service.js";
+import { TOKEN_DICTIONARY, saveKeyValue } from "./services/storage.service.js";
+
+async function saveToken(token) {
+  if (!token.length) {
+    printError("Не передан токен");
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess("Токен сохранен");
+  } catch (error) {
+    printError(error.message);
+  }
+}
 
 function initCLI() {
   const args = getArgs(process.argv);
@@ -11,8 +25,10 @@ function initCLI() {
     pringHelp();
   }
   if (args.t) {
-    saveKeyValue("token", args.t);
+    return saveToken(args.t);
   }
 }
+
+getWeather("krasnoyarsk");
 
 initCLI();
